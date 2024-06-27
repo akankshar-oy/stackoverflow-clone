0ac6e63a7f89eeb467f3f67f6716030fc7f06dc4
akanksha <akankshar610@gmail.com>
2024-06-27 14:27:55 +0530

    first commit

---
+++ client/src/pages/Question/Question.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Leftsidebar from '../../components/Leftsidebar/Leftsidebar'
+import Rightsidebar from '../../components/Rightsidebar/Rightsidebar'
+import Homemainbar from '../../components/Homemainbar/Homemainbar'
+import '../../App.css'
+const Question = ({slidein}) => {
+  return (
+    <div className="home-container-1">
+      <Leftsidebar slidein={slidein}/>
+      <div className="home-container-2">
+        <Homemainbar/>
+        <Rightsidebar/>
+      </div>
+    </div>
+  )
+}
+
+export default Question
---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+import userroutes from "./routes/users.js"
+import questionroutes from "./routes/question.js"
+import answerroutes from "./routes/answer.js"
+
+const app = express();
+dotenv.config();
+app.use(express.json({limit:"30mb", extended:true}))
+app.use(express.urlencoded({limit:"30mb", extended:true}))
+app.use(cors());
+
+
+app.use("/user", userroutes);
+app.use('/questions', questionroutes)
+app.use('/answer',answerroutes)
+app.get('/', (req,res)=>{
+    res.send("Stackoverflow clone is running perfectly")
+})
+
+const PORT= process.env.PORT || 5000
+const database_url=process.env.MONGODB_URL
+
+mongoose.connect(database_url)
+.then(()=> app.listen(PORT, ()=>{console.log(`server running on port ${PORT}`)}))
+.catch((err)=>console.log(err.message))
