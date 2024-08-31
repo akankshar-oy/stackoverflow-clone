76e6b4786673b6ef274313b5b797aef70c38d93f
akankshar-oy <146951245+akankshar-oy@users.noreply.github.com>
2024-08-31 19:54:21 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,24 +5,25 @@ import dotenv from "dotenv"
 import userroutes from "./routes/users.js"
 import questionroutes from "./routes/question.js"
 import answerroutes from "./routes/answer.js"
-
 const app = express();
 dotenv.config();
-app.use(express.json({limit:"30mb", extended:true}))
-app.use(express.urlencoded({limit:"30mb", extended:true}))
+app.use(express.json({ limit: "30mb", extended: true }))
+app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
+
+
 
 
 app.use("/user", userroutes);
 app.use('/questions', questionroutes)
 app.use('/answer',answerroutes)
-app.get('/', (req,res)=>{
-    res.send("Stackoverflow clone is running perfectly")
+app.get('/', (req, res) => {
+    res.send("Stackoverflow is running perfect")
 })
 
-const PORT= process.env.PORT || 5000
-const database_url=process.env.MONGODB_URL
+const PORT = process.env.PORT || 5000
+const database_url = process.env.MONGODB_URL
 
 mongoose.connect(database_url)
-.then(()=> app.listen(PORT, ()=>{console.log(`server running on port ${PORT}`)}))
-.catch((err)=>console.log(err.message))
+    .then(() => app.listen(PORT, () => { console.log(`server running on port ${PORT}`) }))
+    .catch((err) => console.log(err.message))
